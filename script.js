07b9af10e7cf9e125ed72f40b9ee25c9b0b365c2
mynameiskatchy <karchietrinh@gmail.com>
2021-03-17 00:53:36 -0400

    Add functionality for particles to move to upper right

---
+++ script.js
@@ -0,0 +1,39 @@
+disintegrate.init()
+
+document.getElementById('img').addEventListener('click', e => {
+    const disObj = disintegrate.getDisObj(e.target)
+    disintegrate.createSimultaneousParticles(disObj)
+    e.target.remove() 
+})
+
+// function for particles to move to my liking
+const thanosSnap = function () {
+    this.name = 'ThanosSnap'
+    this.animationDuration = 1500
+
+    this.size = 3
+    this.speedX = Math.random()
+    this.speedY = Math.random() * -1 // move up and to right
+    // applied to each particle
+    this.draw = (ctx, percentComplete) => {
+        // start drawing rectangles on our screen
+        ctx.beginPath()
+        // create a rectangle and define dimensions 
+        ctx.fillRect(this.startX - this.size / 2, this.startY - this.size / 2, this.size, this.size)
+        // set colour of rectangle
+        const r = this.rgbArray[0]
+        const g = this.rgbArray[1]
+        const b = this.rgbArray[2]
+        // make squares more and more transpartent
+        const a = 1 - percentComplete // based on how far animation is completed (starts at 0 ends at 1)
+        ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`
+        // draw for us
+        ctx.fill()
+        // move pixels more and more to right as time goes on
+        this.startX += this.speedX
+        this.startY += this.speedY
+        
+    }
+}
+
+disintegrate.addParticleType(thanosSnap)
